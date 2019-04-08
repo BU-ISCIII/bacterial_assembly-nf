864b7d834c6223537f6541ba9fa7e311da78a0c7
Sara M <sara.monzon.fdz@gmail.com>
2019-04-08 13:12:01 +0200

    Added test script and template for docs

---
+++ test/run_test.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+script_path="../main.nf"
+
+if [ -z $1 ]
+then
+    echo "No argument given, going to try to run $script_path"
+else
+    script_path=$1
+fi
+
+data_path="./test_data"
+
+curl --version >/dev/null 2>&1 || { echo >&2 "I require curl, but it's not installed. Aborting."; exit 1; }
+tar --version >/dev/null 2>&1 || { echo >&2 "I require tar, but it's not installed. Aborting."; exit 1; }
+docker -v >/dev/null 2>&1 || { echo >&2 "I require docker, but it's not installed. Visit https://www.docker.com/products/overview#/install_the_platform  ."; exit 1; }
+nextflow -v >/dev/null 2>&1 || { echo >&2 "I require nextflow, but it's not installed. If you hava Java, run 'curl -fsSL get.nextflow.io | bash'. If not, install Java."; exit 1; }
+
+data_dir=${data_path}/test_set
+if [ -d $data_dir ]
+then
+    echo "Found existing test set, using $data_dir"
+else
+	echo "Creating $data_dir"
+	mkdir -p $data_dir
+    echo "Downloading test set..."
+	curl http://starklab.org/data/bardet_natprotoc_2011/input_dmel.fastq.gz -o ${data_dir}/input_dmel.fastq.gz
+	curl http://starklab.org/data/bardet_natprotoc_2011/chip_dmel.fastq.gz -o ${data_dir}/chip_dmel.fastq.gz
+    echo "Downloading reference..."
+    curl ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Drosophila_melanogaster/UCSC/dm6/Drosophila_melanogaster_UCSC_dm6.tar.gz -o ${data_dir}/Drosophila_melanogaster_UCSC_dm6.tar.gz
+    echo "Unpacking reference..."
+    tar -xvf ${data_dir}/Drosophila_melanogaster_UCSC_dm6.tar.gz -C ${data_dir}
+    echo "Done"
+fi
+
+run_name="Test Docker BacterialAssembly Run: "$(date +%s)
+
+nf_cmd="nextflow -C ../nextflow.config run $script_path -resume -name \"$run_name\" --fasta ${data_dir}/genome.fa --gtf ${data_dir}/genes.gtf --reads \"${data_dir}/*.fastq.gz\" --outdir ./${data_path}/results"
+echo "Starting nextflow... Command:"
+echo $nf_cmd
+echo "--------------------------------------------------"
+eval $nf_cmd
+
